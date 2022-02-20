c38be1a67a0bf8915681c54111482b18c947a825
Mr-Gapo <tigran.7t@mail.ru>
2022-02-20 19:54:08 +0400

    chupak chupak

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 // https://p5js.org/reference/
 
 var screen = 600
+var fps = 60
+var x = 20
+var y = 20
+
+
 function setup() {
     createCanvas(screen,screen);
-
+    frameRate(fps)
 }
 
 function draw() {
     background(80,80,80);
-
+    fill(255,0,0)
+    rect(x,y,20,20)
+    if (x >= screen && y >= screen){
+        x--
+        y--
+    }
+    else if (x <= screen && y <= screen) {
+        x++
+        y++
+        
+    }
 }
